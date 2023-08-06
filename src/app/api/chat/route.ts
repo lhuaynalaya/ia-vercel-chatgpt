7e7c0e0bc6a8a60ddcd529ace1ce2acf8878e93f
Luis Fernando <lhuaynalayag@gmail.com>
2023-08-05 23:19:44 -0500

    sdk

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,13 +1,12 @@
 import { Configuration, OpenAIApi } from 'openai-edge';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
-import { NextResponse } from 'next/server';
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
 
 const openai = new OpenAIApi(config);
-
+console.log('openai', openai);
 export async function POST(request: Request) {
   const {messages} = await request.json();
 
